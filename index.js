e56c5857a0e1653c23ebc03a628ae3e7582412ac
bsc-com-22-20 <bsc-com-22-20@unima.ac.mw>
2023-04-13 02:04:46 +0300

    this is a project that is successfully querrying to the database

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 const express = require('express')
 const app = express()
+const bodyParser = require("body-parser");
 
-const post = require('./src/posts/post.models')
+const dbConnection = require('./src/utils/mysql.connector');
+const post = require("./src/posts/post.models");
 
-qpp.get('/', function(req, res)){
-    return res.json(req)
-}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.get('/', function(req, res){
+    return res.json(req.headers)
+})
 
 app.get('/api/v1/posts' , function(req,res) {
-    return res.json(post)
+    let sql = "select * from posts"
+    return dbConnection.query(sql, function(err, results){
+        if (err) throw err;
+    return res.json(results);
 
+    })
+    
+})
+
+app.post("/api/v1/posts", (req, res) => {
+    console.log(req.body);
+    const { name, imageurl, summary} = req.body;
+    let sql = `INSERT INTO POSTS ( name, imageurl, summary) VALUES ('${name}', '${imageurl}', '${summary}')`;
+    dbConnection.query(sql, (err) => {
+        if (err) throw err;
+        console.log("1 insert recorded");
+
+    })
 })
 app.listen(3000, function(){
     console.log('OnlineRestaurant listening on port 3000')
-})
+    // let sql = "INSERT INTO posts (name, imageurl, summary) VALUES ( 'khumula', 'picture', 'conclusion')"
+    // dbConnection.query(sql,function(err){
+    //     if (err) throw err 
+    //     console.log ("one insert recorded")
+    // })
+
+    dbConnection.connect(function(err){
+         if (err) throw err
+ 
+        console.log("connected to MySQL")
+        
+    })
+
+   })
---
+++ src/utils/mysql.connector.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const config = {
+    host: "localhost",
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    connectionLimit: 100,
+
+
+}
+
+const connection = mysql.createConnection(config)
+
+module.exports = connection
