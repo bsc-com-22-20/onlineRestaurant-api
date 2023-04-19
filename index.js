07cb5542ca8eee41f8694ccb72e97d62149fdd5f
bsc-com-22-20 <bsc-com-22-20@unima.ac.mw>
2023-04-19 18:24:38 +0300

    getting a post from the database using an id and a PATCH endpoint

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ const bodyParser = require("body-parser");
 
 const dbConnection = require('./src/utils/mysql.connector');
 const post = require("./src/posts/post.models");
+ const { response } = require('express');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
+app.patch('/api/v1/posts/:id', function(request, response){
+    //console.log(request.body, request.params)
+
+    const sql = `SELECT * FROM posts WHERE id =${request.params.id} LIMIT 1`
+
+    return dbConnection.query(sql, function(err, rows) {
+        if(err) throw err
+
+        return response.json(rows)
+    })
+})
+
 app.get('/', function(req, res){
     return res.json(req.headers)
-})
+}) 
 
 app.get('/api/v1/posts' , function(req,res) {
     let sql = "select * from posts"
@@ -30,21 +43,21 @@ app.post("/api/v1/posts", (req, res) => {
         if (err) throw err;
         console.log("1 insert recorded");
 
-    })
+     })
 })
 app.listen(3000, function(){
     console.log('OnlineRestaurant listening on port 3000')
-    // let sql = "INSERT INTO posts (name, imageurl, summary) VALUES ( 'khumula', 'picture', 'conclusion')"
-    // dbConnection.query(sql,function(err){
-    //     if (err) throw err 
-    //     console.log ("one insert recorded")
-    // })
+     /* let sql = "INSERT INTO posts (name, imageurl, summary) VALUES ( 'angie', 'picture', 'conclusion')"
+     dbConnection.query(sql,function(err){
+         if (err) throw err 
+         console.log ("one insert recorded")
+    }) */
 
-    dbConnection.connect(function(err){
+     dbConnection.connect(function(err){
          if (err) throw err
  
         console.log("connected to MySQL")
         
-    })
+    }) 
 
    })
